732d52c82da6cddb1210c7d62bccec7f246b6240
taneda <1335590892@qq.com>
2019-03-15 09:15:20 +0800

    Add database support with Flask-SQLAlchemy

---
+++ forge.py
@@ -0,0 +1,33 @@
+import click
+from app import Movie, User, db
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Gery li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+
+    db.session.commit()
+    click.echo('Done.')
